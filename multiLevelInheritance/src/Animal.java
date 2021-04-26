c1ea335993d72cab5cfcd44455a40046e082ccb1
Rajib Home <asru.sajol@gmail.com>
2021-04-26 08:11:55 +0600

    Multilevel Inheritance

--- multiLevelInheritance/src/Animal.java
+++ multiLevelInheritance/src/Animal.java
@@ -10,15 +10,34 @@ class Dog extends Animal{
 }
 
 class BabyDog extends Dog{
-    void weep(){
+    int weep(){
         System.out.println("Weeping ....");
+        return 0;
     }
 }
 class TestInheritance{
     public static void main(String[] args) {
-        BabyDog dog = new BabyDog();
+        BabyDog dog = new BabyDog();  //casting
         dog.weep();
         dog.bark();
         dog.eat();
+
+        int a = 4;
+        long b = 10l;
+        float c = 20.5f;
+        double d = 50.0 ;
+
+        //a =(int) b;
+        b = a;
+        String  s = "1234";
+        Integer h = 40;
+       int k = Integer.parseInt(s);
+        System.out.println(k);
+       // System.out.println();
     }
+
 }
+// casting for int,long flot, double,
+//String Methods ,
+// arry,loop, list, map, thread, multiThread,\
+// ** redis, RabbitMQ, mySQL(steps)
